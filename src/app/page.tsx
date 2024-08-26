f94e4316d7b3f17e2faa9371744df696ac1ebf32
linamkrtumyan <mkrtumyanlina@gmail.com>
2024-08-26 18:03:09 +0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Image from "next/image";
+import styles from "./page.module.css";
+import { TelegramConnect } from "./TelegramConnect";
+
+export default function Home() {
+  let name = "ex_test_bot"; // Это имя бота который вы ранее создавали в BotFather
+  const handleBot = (user: any) => {
+    console.log(user);
+  };
+  return (
+    <main className={styles.main}>
+      <TelegramConnect
+        botName={name}
+        buttonSize="large" // "large" | "medium" | "small"
+        cornerRadius={3} // 0 - 20
+        usePic={false} // true | false
+        dataOnauth={handleBot}
+      />
+    </main>
+  );
+}
