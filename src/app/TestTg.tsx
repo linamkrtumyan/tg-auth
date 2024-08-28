fd18b6ce45eac2f100d39979d3c1811d43012a50
linamkrtumyan <mkrtumyanlina@gmail.com>
2024-08-28 11:32:50 +0400

    styles13

---
+++ src/app/TestTg.tsx
@@ -0,0 +1,128 @@
+"use client"
+import React, { useRef, useEffect, useState } from "react";
+import "./styles.css";
+
+interface TelegramLoginButtonProps {
+  botName: string;
+  buttonSize?: "large" | "medium" | "small";
+  className?: string;
+  cornerRadius?: string;
+  requestAccess?: string;
+  usePic?: boolean;
+  dataOnauth?: (user: any) => void;
+  dataAuthUrl?: string;
+  lang?: string;
+}
+
+const TelegramLoginButton: React.FC<TelegramLoginButtonProps> = ({
+  botName,
+  buttonSize = "large",
+  className = "",
+  cornerRadius,
+  requestAccess = "write",
+  usePic = false,
+  dataOnauth,
+  dataAuthUrl,
+  lang = "en",
+}) => {
+  const [loader, setLoader] = useState(false);
+  const instance = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (instance.current) {
+      (window as any).TelegramLoginWidget = {
+        dataOnauth: (user: any) => {
+          if (dataOnauth) {
+            dataOnauth(user);
+          }
+        },
+      };
+      const script = document.createElement("script");
+      script.src = "https://telegram.org/js/telegram-widget.js?14";
+      script.setAttribute("data-telegram-login", botName);
+      script.setAttribute("data-size", buttonSize);
+      if (cornerRadius) {
+        script.setAttribute("data-radius", cornerRadius);
+      }
+      script.setAttribute("data-request-access", requestAccess);
+      script.setAttribute("data-userpic", usePic ? "true" : "false");
+      script.setAttribute("data-lang", lang);
+
+      if (dataAuthUrl) {
+        script.setAttribute("data-auth-url", dataAuthUrl);
+      } else {
+        script.setAttribute(
+          "data-onauth",
+          "TelegramLoginWidget.dataOnauth(user)"
+        );
+      }
+      script.async = true;
+
+      instance.current.appendChild(script);
+      setTimeout(() => {
+        setLoader(true);
+      }, 1000);
+    }
+  }, [botName, buttonSize, cornerRadius, requestAccess, usePic, dataAuthUrl, lang, dataOnauth]);
+
+  return (
+    <div className="mainclass">
+      <div className="tg-main">
+        {loader ? (
+          <div className="tg-logo1">
+            <img
+              style={{ width: "18px", height: "18px" }}
+              src="https://static-00.iconduck.com/assets.00/telegram-icon-512x435-4ugjo4i4.png"
+              alt="Telegram Logo"
+            />
+            <p
+              style={{
+                paddingLeft: "10px",
+                color: "white",
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              Telegram
+            </p>
+          </div>
+        ) : (
+          <div
+            style={{
+              height: "28px",
+              width: "200px",
+              justifySelf: "center",
+            }}
+          >
+            <p
+              style={{
+                color: "black",
+                fontWeight: "bold",
+              }}
+            >
+              Loading...
+            </p>
+          </div>
+        )}
+        <div ref={instance} className={className} />
+      </div>
+    </div>
+  );
+};
+
+export default TelegramLoginButton;
+
+const onAuth = (ctx: any) => {
+  console.log(ctx);
+};
+
+export const BotButton: React.FC = () => {
+  return (
+    <TelegramLoginButton
+      botName="exlina_test_bot"
+      cornerRadius="4"
+      className="tg-login-button"
+      dataOnauth={onAuth}
+    />
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { TelegramConnect } from "./TelegramConnect";
+import { BotButton } from "./TestTg";
 
 export default function Home() {
   let name = "exlina_test_bot"; // Это имя бота который вы ранее создавали в BotFather
@@ -10,7 +11,7 @@ export default function Home() {
   };
   return (
     <main className={styles.main}>
-      <TelegramConnect
+      {/* <TelegramConnect
         botName={name}
         buttonSize="large" // "large" | "medium" | "small"
         cornerRadius={14} // 0 - 20
@@ -18,7 +19,9 @@ export default function Home() {
         dataOnauth={handleBot}
         className={styles.telegramBtn}
         
-      />
+      /> */}
+
+      <BotButton/>
     </main>
   );
 }
